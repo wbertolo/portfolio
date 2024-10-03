559c448f585893e9b4d49ffa831f488afaa4fdeb
LOFTMUSIC\willb <loftmusic.ca@gmail.com>
2024-10-03 16:55:07 -0400

    updates

---
+++ portfolio/src/app/ui/card.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Cta from './cta';
+
+
+export default function Card(params:any) {
+	return (
+		<div 
+			className={`basis-[32%] inline-block mb-[20px] mx-0 min-h-[184px]
+			flex flex-col p-4 bg-slate-800 border border-slate-900 h-full opacity-90 hover:opacity-100
+			transition-shadow duration-500 hover:shadow-2xl hover:shadow-amber-500`}
+		>
+			<Link href={params.href} target='_blank'>
+			<div className="mb-8">
+				<Image
+					src={params.imgSrc}
+					width={640}
+					height={360}
+					alt={params.imgAlt}
+				/>
+			</div>
+			</Link>
+			<div>
+				<Link href={params.href} target='_blank'><h2 className="mt-0 mb-3">{params.title}</h2></Link>
+				<Link href={params.href} target='_blank'><p className="mb-4 grow">{params.description}</p></Link>
+				<Cta ctaText={params.ctaText} href={params.href} />
+			</div>
+		</div>
+	)
+}
+  
--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -9,8 +9,12 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'blue-light': '#9fe8f6',
+        'blue-medium': '#00414f',
+        'blue-dark': '#002830',
+        'purple-medium': '#66229f',
+        'purple-medium-2': '#2f0850',        
+        'purple-light': '#d3b5f1',
       },
     },
   },
