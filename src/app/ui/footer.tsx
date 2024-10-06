7049fd356be6011c693e2587f396f724709fdd79
LOFTMUSIC\willb <loftmusic.ca@gmail.com>
2024-10-06 11:15:41 -0400

    updates

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
 	return (
 		<footer className="w-full flex fixed bottom-0 py-2 px-0 justify-center bg-[#111] text-slate-100">
 			<div className="w-full flex max-w-[960px] justify-between py-1 px-3 lg:py-2 lg:px-0">
-				<p class="m-0">&copy; 2024, William Bertolo</p>
+				<p className="m-0">&copy; 2024, William Bertolo</p>
 				<Link 
 					href="https://pagespeed.web.dev/analysis/https-william-bertolo-netlify-app/up31bcbazr?form_factor=mobile"
 					target="_blank"
