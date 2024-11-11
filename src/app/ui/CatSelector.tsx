e82fde1871f3e8fbc65d6639e7cde29f019fe57a
Will <willb.can@gmail.com>
2024-11-11 11:22:38 -0500

    updates

---
+++ src/app/ui/CatSelector.tsx
@@ -0,0 +1,16 @@
+export default function CatSelector( params:any ) {
+
+    return (
+        <select 
+            className="bg-gray-700 border border-gray-600 text-sm focus:ring-blue-500 focus:border-blue-500 block placeholder-gray-400 text-white p-2.5 mb-5 mx-auto"
+            onChange={function(e) {params.setCategory(e.target.value)}}
+            aria-label="Industry"
+        >
+            <option value="All">All</option>
+            <option value="Electronics">Electronics</option>
+            <option value="Media">Media</option>
+            <option value="Music">Music</option>
+            <option value="SaaS">Sass</option>
+        </select>
+    )
+}
