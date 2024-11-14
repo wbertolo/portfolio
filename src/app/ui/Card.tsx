5492c4b2ac30da03998d003842380345e432483f
Will <willb.can@gmail.com>
2024-11-14 18:18:23 -0500

    updates

--- src/app/ui/Card.tsx
+++ src/app/ui/Card.tsx
@@ -7,107 +7,19 @@ export default function Card(params:any) {
 	const [openState, setOpenState] = useState(false);
 
 	const handleClick = () => {
-
 		setOpenState(!openState);
-
-
-		setTimeout(
-			function() {
-
-				if (!openState) {
-
-					document.querySelector(`#card-${params.id}`)?.classList.remove('closed');
-					document.querySelector(`#card-${params.id}`)?.classList.add('open');
-
-					setTimeout(
-						function() {
-							if (!openState) {
-
-								document.getElementById(`card-${params.id}`)?.scrollIntoView(
-									{ 
-										block:"center",
-										inline: "center",
-										behavior: "smooth"
-									}
-								);
-
-								setTimeout(
-									function() {
-										document.querySelector(`#card-${params.id} .description`)?.classList.remove('desc-closed');
-										document.querySelector(`#card-${params.id} .description`)?.classList.add('desc-open');
-									},
-									500
-								);
-
-							}
-						}, 
-						500
-					);
-
-
-				} else {
-					document.querySelector(`#card-${params.id} .description`)?.classList.remove('desc-open');
-					document.querySelector(`#card-${params.id} .description`)?.classList.add('desc-closed');
-
-					setTimeout(
-						function() {
-							document.querySelector(`#card-${params.id}`)?.classList.remove('open');
-							document.querySelector(`#card-${params.id}`)?.classList.add('closed');
-						}, 
-						500
-					);
-
-				}
-
-
-			}, 
-			300
-		);
 	}
 
 	return (
 		<>
-			<style jsx>{`
-				.card {
-					transition: flex-basis 0.75s ease;
-				}
-				.open {
-
-					flex-basis: 100%;
-
-					@media only screen and (min-width: 768px) {
-						flex-basis: 64%;
-					}
-				
-
-					.description {
-						transition: all 0.3s ease;
-					}
-				}
-
-				.closed {
-					flex-basis: 100%;
-
-					@media only screen and (min-width: 768px) {
-						flex-basis: 31%;
-					}
-				}
-
-				.desc-closed {
-					max-height: 0;
-				}
-
-				.desc-open {
-					max-height: 500px;
-				}
-			`}</style>
 			<div 
 				id={`card-${params.id}`}
-				className={`card closed cursor-pointer text-white inline-block mb-[20px] mx-2 min-h-[184px] 
-				flex flex-col justify-between p-4 bg-slate-800 border border-slate-900 h-full opacity-90 hover:opacity-100
-				transition-all duration-1000 ease hover:shadow-2xl hover:shadow-purple-500`}
+				className={`card cursor-pointer text-white inline-block mb-[20px] mx-2 min-h-[184px] 
+				flex flex-col justify-between basis-full md:basis-[31%] p-4 bg-slate-800 border border-slate-900 h-full 
+				opacity-90 hover:opacity-100 transition-all duration-75 ease hover:shadow-2xl hover:shadow-purple-500`}
 				onClick={handleClick}
 			>
+				
 				<Image
 					src={params.imgSrc}
 					width={294}
@@ -117,7 +29,7 @@ export default function Card(params:any) {
 				/>
 				<div>
 					<h2 className="mt-0 mb-3 text-white">{params.name}</h2>
-					<div id="description" className="description desc-closed mb-4 transition-all ease duration-200 overflow-hidden">
+					<div id="description" className={`description ${openState ? 'max-h-[500px]' : 'max-h-0'} mb-4 transition-all ease duration-200 overflow-hidden`}>
 						<p>{params.description}</p>
 
 						<Link 
@@ -130,7 +42,7 @@ export default function Card(params:any) {
 						</Link>
 
 					</div>
-					<div className="mb-4">Category: {params.category}</div>
+					<div className="mb-4">{openState}Category: {params.category}</div>
 				</div>
 			</div>
 		</>
